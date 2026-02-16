75b6cccfa4160ffbad175c782d4055c67e7069b6
EnoahGrey <enoahgrey@gmail.com>
2026-02-15 18:40:45 -0600

    Finished Break Timer

---
+++ Custom/TimerLogic.cs
@@ -0,0 +1,35 @@
+namespace BreakTime.Custom;
+
+public class TimerLogic
+{
+    private int _intSec;
+    public int _intMin;
+
+    public void SetBreakTime(int time)
+    {
+        if (time == 5) {
+            _intMin = 5;
+        }
+        else if (time == 10) {
+            _intMin = 10;
+        }
+        else {
+            _intMin = 15;
+        }
+    }
+    
+    public void SetTickCount()
+    {
+        _intSec++;
+        if (_intSec == 60)
+        {
+            _intMin--;
+            _intSec = 0;
+        }
+    }
+    
+    public string MinLeftMessage()
+    {
+       return _intMin.ToString().PadLeft(2, ' ') + " Minutes Left";  
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using BreakTime.Custom;
+
+namespace BreakTime;
+
+public partial class MainPage : ContentPage
+{
+    TimerLogic oTimer = new TimerLogic();
+    private bool isRunning = false;
+    private bool isWhite = true;
+    
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "Break Time";
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        btnTake5.IsEnabled = true;
+        btnTake10.IsEnabled = true;
+        btnTake15.IsEnabled = true;
+        btnStop.IsEnabled = false;
+    }
+
+    private void BtnTake5_OnClicked(object sender, EventArgs e)
+    {
+        btnTake5.IsEnabled = false;
+        btnTake10.IsEnabled = false;
+        btnTake15.IsEnabled = false;
+        btnStop.IsEnabled = true;
+        
+        oTimer.SetBreakTime(5);
+        isRunning = true;
+
+            Application.Current.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                if (isRunning)
+                {
+                    if (oTimer._intMin > 0)
+                    {
+                        oTimer.SetTickCount(); 
+                        lblDisplay.Text = oTimer.MinLeftMessage();
+                    }
+                    else
+                    {
+                        lblDisplay.Text = "Time is Up!";
+                        if (isWhite)
+                        {
+                            ftmMain.Background = Colors.Red;
+                            isWhite = false;
+                        }
+                        else
+                        {
+                            ftmMain.Background = Colors.White;
+                            isWhite = true;
+                        }
+                    }
+                }
+                return isRunning;
+            });
+    }
+
+    private void BtnTake10_OnClicked(object sender, EventArgs e)
+    {
+        btnTake5.IsEnabled = false;
+        btnTake10.IsEnabled = false;
+        btnTake15.IsEnabled = false;
+        btnStop.IsEnabled = true;
+            isRunning = true;
+
+            Application.Current.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                if (isRunning)
+                {
+                    oTimer.SetBreakTime(10);
+                    oTimer.SetTickCount();
+                    lblDisplay.Text = oTimer.MinLeftMessage();
+                }
+                return isRunning;
+            });
+    }
+
+    private void BtnTake15_OnClicked(object sender, EventArgs e)
+    {
+        btnTake5.IsEnabled = false;
+        btnTake10.IsEnabled = false;
+        btnTake15.IsEnabled = false;
+        btnStop.IsEnabled = true;
+            isRunning = true;
+
+            Application.Current.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                if (isRunning)
+                {
+                    oTimer.SetBreakTime(15);
+                    oTimer.SetTickCount();
+                    lblDisplay.Text = oTimer.MinLeftMessage();
+                }
+                return isRunning;
+            });
+    }
+
+    private void BtnStop_OnClicked(object sender, EventArgs e)
+    {
+        btnTake5.IsEnabled = true;
+        btnTake10.IsEnabled = true;
+        btnTake15.IsEnabled = true;
+        btnStop.IsEnabled = false;
+        isRunning = false;
+        isWhite = true;
+    }
+}
